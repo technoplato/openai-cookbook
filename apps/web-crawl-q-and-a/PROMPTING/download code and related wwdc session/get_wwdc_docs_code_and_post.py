05674128816ea3d003c230601038ec0155b2cdcf
Michael Lustig <lustig@knophy.com>
2023-08-03 15:35:30 -0400

    ❌ Trying to utlize db
     python PROMPTING/download\ code\ and\ related\ wwdc\ session/get_wwdc_docs_code_and_post.py
    ################################################################################
    ##### Starting URL: https://developer.apple.com/sample-code/wwdc/2023/
    ################################################################################
    ################################################################################
    ##### Processing WWDC year: WWDC23
    ################################################################################
    Traceback (most recent call last):
      File "/Users/laptop/Development/01-Learning/oai/openai-cookbook/apps/web-crawl-q-and-a/PROMPTING/download code and related wwdc session/get_wwdc_docs_code_and_post.py", line 120, in <module>
        main()
      File "/Users/laptop/Development/01-Learning/oai/openai-cookbook/apps/web-crawl-q-and-a/PROMPTING/download code and related wwdc session/get_wwdc_docs_code_and_post.py", line 113, in main
        view_code_urls = get_view_code_urls(driver, wwdc_year, wwdc_url, db)
      File "/Users/laptop/Development/01-Learning/oai/openai-cookbook/apps/web-crawl-q-and-a/PROMPTING/download code and related wwdc session/get_wwdc_docs_code_and_post.py", line 97, in get_view_code_urls
        if not db.is_page_downloaded(url_id):
    AttributeError: 'WWDCDatabase' object has no attribute 'is_page_downloaded'. Did you mean: 'mark_page_downloaded'?

--- apps/web-crawl-q-and-a/PROMPTING/download code and related wwdc session/get_wwdc_docs_code_and_post.py
+++ apps/web-crawl-q-and-a/PROMPTING/download code and related wwdc session/get_wwdc_docs_code_and_post.py
@@ -3,6 +3,59 @@ from selenium.webdriver.chrome.options import Options
 from selenium import webdriver
 from datetime import datetime
 
+import sqlite3
+
+
+class WWDCDatabase:
+    def __init__(self, db_path="wwdc_data.db"):
+        self.conn = sqlite3.connect(db_path)
+        self.initialize_database()
+
+    def initialize_database(self):
+        with self.conn:
+            self.conn.execute("""
+                CREATE TABLE IF NOT EXISTS urls (
+                    id INTEGER PRIMARY KEY,
+                    wwdc_year TEXT,
+                    url_type TEXT,
+                    url TEXT,
+                    page_downloaded BOOLEAN DEFAULT 0,
+                    wwdc_links_scraped BOOLEAN DEFAULT 0,
+                    transcript_downloaded BOOLEAN DEFAULT 0,
+                    overview_downloaded BOOLEAN DEFAULT 0,
+                    code_downloaded BOOLEAN DEFAULT 0,
+                    updated_at TIMESTAMP
+                )
+            """)
+
+    def save_url(self, wwdc_year, url_type, url):
+        with self.conn:
+            self.conn.execute("""
+                INSERT INTO urls (wwdc_year, url_type, url, updated_at) VALUES (?, ?, ?, ?)
+            """, (wwdc_year, url_type, url, datetime.now()))
+
+    def mark_page_downloaded(self, url_id):
+        self._update_status_flag(url_id, "page_downloaded")
+
+    def mark_wwdc_links_scraped(self, url_id):
+        self._update_status_flag(url_id, "wwdc_links_scraped")
+
+    def mark_transcript_downloaded(self, url_id):
+        self._update_status_flag(url_id, "transcript_downloaded")
+
+    def mark_overview_downloaded(self, url_id):
+        self._update_status_flag(url_id, "overview_downloaded")
+
+    def mark_code_downloaded(self, url_id):
+        self._update_status_flag(url_id, "code_downloaded")
+
+    def _update_status_flag(self, url_id, flag_column):
+        with self.conn:
+            self.conn.execute(f"""
+                UPDATE urls SET {flag_column} = 1, updated_at = ? WHERE id = ?
+            """, (datetime.now(), url_id))
+
+
 # ####################################################################################################
 # Setup WebDriver and Chrome Options
 # ####################################################################################################
@@ -12,57 +65,57 @@ chrome_options = Options()
 chrome_options.binary_location = chrome_binary_path
 driver = webdriver.Chrome(
     executable_path=chromedriver_path, options=chrome_options)
-
 # ####################################################################################################
-# Get the Current Year and Construct the Starting URL
-# ####################################################################################################
-current_year = datetime.now().year
-starting_url = f"https://developer.apple.com/sample-code/wwdc/{current_year}/"
-print(f"Starting URL: {starting_url}")
-
-# ####################################################################################################
-# Function to Get "View Code" URLs
+# Setup WebDriver and Chrome Options - END
 # ####################################################################################################
 
 
-def get_view_code_urls(soup):
-    view_code_urls = []
-    view_code_links = soup.find_all("a", string="View code")
-    for link in view_code_links:
-        url = link.get("href")
-        if url:
-            view_code_urls.append(url)
-    return view_code_urls
+def comment(message):
+    fence = '#' * 80
+    print(fence)
+    print(f"##### {message}")
+    print(fence)
 
 
-# ####################################################################################################
-# Navigate to the Starting URL and Extract WWDC Full URLs
-# ####################################################################################################
-driver.get(starting_url)
-driver.implicitly_wait(10)
-html_content = driver.page_source
-soup = BeautifulSoup(html_content, "html.parser")
-wwdc_links = soup.select(".localnav-menu-items a[href*='/sample-code/wwdc/']")
-for link in wwdc_links:
-    wwdc_text = link.text.strip()
-    if wwdc_text in ["WWDC23", "WWDC22", "WWDC21", "WWDC20"]:
-        wwdc_href = link.get("href")
-        full_url = f"https://developer.apple.com{wwdc_href}"
-        print(f"Processing {wwdc_text}: {full_url}")
+def get_current_year_url():
+    current_year = datetime.now().year
+    return f"https://developer.apple.com/sample-code/wwdc/{current_year}/"
 
-        # Navigate to the WWDC year's URL
-        driver.get(full_url)
-        driver.implicitly_wait(10)
-        year_html_content = driver.page_source
-        year_soup = BeautifulSoup(year_html_content, "html.parser")
 
-        # Get and print the "View Code" URLs
-        view_code_urls = get_view_code_urls(year_soup)
-        print(f"Found {len(view_code_urls)} 'View Code' URLs for {wwdc_text}")
+def get_wwdc_urls(driver, starting_url):
+    driver.get(starting_url)
+    driver.implicitly_wait(10)
+    html_content = driver.page_source
+    soup = BeautifulSoup(html_content, "html.parser")
+    wwdc_links = soup.select(
+        ".localnav-menu-items a[href*='/sample-code/wwdc/']")
+    return [(link.text.strip(), f"https://developer.apple.com{link.get('href')}") for link in wwdc_links]
+
+
+def get_view_code_urls(driver, wwdc_year, wwdc_url, db):
+    url_id = db.save_url(wwdc_year, "View code", wwdc_url)
+    if not db.is_page_downloaded(url_id):
+        comment("Downloading the page")
+        # Code to download the page
+        db.mark_page_downloaded(url_id)
+    comment("Code to process the page if needed")
+    # Code to process the page if needed
+    # ...
+
+
+def main():
+    db = WWDCDatabase()
+    starting_url = get_current_year_url()
+    comment(f"Starting URL: {starting_url}")
+    wwdc_urls = get_wwdc_urls(driver, starting_url)
+    for wwdc_year, wwdc_url in wwdc_urls:
+        comment(f"Processing WWDC year: {wwdc_year}")
+        view_code_urls = get_view_code_urls(driver, wwdc_year, wwdc_url, db)
         for url in view_code_urls:
-            print(url)
+            comment(f"Processing URL: {url}")
+            # get_view_code_urls(driver, url, db)
 
-# ####################################################################################################
-# Close WebDriver
-# ####################################################################################################
-driver.quit()
+
+if __name__ == "__main__":
+    main()
+    driver.quit()
