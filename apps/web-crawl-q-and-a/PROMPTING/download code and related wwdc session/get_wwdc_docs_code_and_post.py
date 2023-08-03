028085da64bf4e934c8e7ddf9d1587ac84b72e4d
Michael Lustig <lustig@knophy.com>
2023-08-03 15:02:15 -0400

    ❌ attempt #1 get all years

--- apps/web-crawl-q-and-a/PROMPTING/download code and related wwdc session/get_wwdc_docs_code_and_post.py
+++ apps/web-crawl-q-and-a/PROMPTING/download code and related wwdc session/get_wwdc_docs_code_and_post.py
@@ -35,9 +35,57 @@ html_content = driver.page_source
 # Parse the HTML content using BeautifulSoup
 soup = BeautifulSoup(html_content, "html.parser")
 
-# Function to get the "View Code" URLs
+# ####################################################################################################
+# Function to Get "View Code" URLs
+# ####################################################################################################
 
 
+# def get_view_code_urls(soup):
+#     view_code_urls = []
+#     view_code_links = soup.find_all("a", string="View code")
+#     for link in view_code_links:
+#         url = link.get("href")
+#         if url:
+#             view_code_urls.append(url)
+#     return view_code_urls
+
+
+# # Get the "View Code" URLs
+# view_code_urls = get_view_code_urls(soup)
+
+# # ####################################################################################################
+# # Visit First "View Code" URL and Extract Title, Description, Platforms, and WWDC Sessions
+# # ####################################################################################################
+
+# # Visit the first "View code" URL
+# first_view_code_url = view_code_urls[0]
+# driver.get(first_view_code_url)
+
+# # Extract the title, description, and platforms
+# title = driver.find_element_by_css_selector(".cs-title").text
+# description = driver.find_element_by_css_selector(".cs-overview").text
+# platforms = [span.text for span in driver.find_elements_by_css_selector(
+#     ".platform-container span")]
+
+# # Find and print the linked WWDC sessions
+# wwdc_session_links = driver.find_elements_by_partial_link_text("WWDC")
+# wwdc_session_urls = [link.get_attribute("href") for link in wwdc_session_links]
+
+# # Print the extracted information
+# print("Title:", title)
+# print("Description:", description)
+# print("Platforms:", platforms)
+# print("WWDC Sessions:", wwdc_session_urls)
+
+# # ####################################################################################################
+# # Close WebDriver
+# # ####################################################################################################
+# # Close the WebDriver
+# driver.quit()
+
+# ####################################################################################################
+# Function to Get "View Code" URLs
+# ####################################################################################################
 def get_view_code_urls(soup):
     view_code_urls = []
     view_code_links = soup.find_all("a", string="View code")
@@ -48,35 +96,32 @@ def get_view_code_urls(soup):
     return view_code_urls
 
 
-# Get the "View Code" URLs
-view_code_urls = get_view_code_urls(soup)
+# ####################################################################################################
+# Navigate to Sample Code Page and Extract Available Years
+# ####################################################################################################
+base_url = "https://developer.apple.com/sample-code/wwdc/"
+driver.get(base_url)
+driver.implicitly_wait(10)
+html_content = driver.page_source
+soup = BeautifulSoup(html_content, "html.parser")
+years_links = soup.select(".localnav-menu-items .localnav-menu-link")
+years_urls = [link.get("href") for link in years_links]
 
 # ####################################################################################################
-# Visit First "View Code" URL and Extract Title, Description, Platforms, and WWDC Sessions
+# Iterate Through Available Years and Navigate to Sample Code Page for Each Year
 # ####################################################################################################
+for year_url in years_urls:
+    print(f"Processing {year_url}")
+    driver.get(base_url + year_url)
+    driver.implicitly_wait(10)
+    html_content = driver.page_source
+    soup = BeautifulSoup(html_content, "html.parser")
+    view_code_urls = get_view_code_urls(soup)
+    print(f"Found {len(view_code_urls)} 'View Code' URLs for {year_url}")
 
-# Visit the first "View code" URL
-first_view_code_url = view_code_urls[0]
-driver.get(first_view_code_url)
-
-# Extract the title, description, and platforms
-title = driver.find_element_by_css_selector(".cs-title").text
-description = driver.find_element_by_css_selector(".cs-overview").text
-platforms = [span.text for span in driver.find_elements_by_css_selector(
-    ".platform-container span")]
-
-# Find and print the linked WWDC sessions
-wwdc_session_links = driver.find_elements_by_partial_link_text("WWDC")
-wwdc_session_urls = [link.get_attribute("href") for link in wwdc_session_links]
-
-# Print the extracted information
-print("Title:", title)
-print("Description:", description)
-print("Platforms:", platforms)
-print("WWDC Sessions:", wwdc_session_urls)
+    # You can add more code here to visit each "View code" URL and extract information for the year
 
 # ####################################################################################################
 # Close WebDriver
 # ####################################################################################################
-# Close the WebDriver
 driver.quit()
